46a47349b022549cda232f6da013646af88cf19c
Balavinayagam Shanmugavel <itbalan@yahoo.com>
2022-12-01 18:44:11 +0800

    Commit changes

---
+++ pages/_document.js
@@ -0,0 +1,56 @@
+/* eslint-disable @next/next/no-page-custom-font */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/types/create-instance';
+import createCache from '@emotion/cache';
+import { style } from '@mui/system';
+
+export default class MyDocument extends Document {
+  render() {
+    <Html lang="en">
+      // eslint-disable-next-line react/jsx-no-comment-textnodes,
+      react/jsx-no-comment-textnodes, react/jsx-no-comment-textnodes,
+      react/jsx-no-comment-textnodes
+      <Head>
+        // eslint-disable-next-line @next/next/no-page-custom-font,
+        @next/next/no-page-custom-font
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>;
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createCache({ key: 'css' });
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emtion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ _html: style.css }}
+    />
+  ));
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
